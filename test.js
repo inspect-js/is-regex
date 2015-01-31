2ebef9f0759843e9a063de7a512b46e3e7daea7e
Jordan Harband <ljharb@gmail.com>
2015-01-31 10:20:39 -0800

    Fix tests for faked @@toStringTag

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ test('not regexes', function (t) {
 test('@@toStringTag', { skip: !hasToStringTag }, function (t) {
 	var regex = /a/g;
 	var fakeRegex = { valueOf: function () { return regex; }, toString: function () { return String(regex); } };
-	fakeRegex[Symbol.toStringTag] = function () { return 'RegExp'; };
+	fakeRegex[Symbol.toStringTag] = 'RegExp';
 	t.notOk(isRegex(fakeRegex), 'fake RegExp with @@toStringTag "RegExp" is not regex');
 	t.end();
 });
